7292ba4c741134c8218808618be742065849485d
Lan Tran <lan.tran@quincus.com>
2024-07-28 17:23:09 +0700

    added our games section

--- src/components/OurGames/index.tsx
+++ src/components/OurGames/index.tsx
@@ -1,9 +1,113 @@
-import React from 'react'
+import React from "react";
+import SectionHeader from "../SectionHeader";
+import Image from "next/image";
 
 const OurGames = () => {
+  const data = [
+    {
+      id: 1,
+      title: "E-Space",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      bgUrl: "/assets/images/our_games/1_e_space.png",
+    },
+    {
+      id: 2,
+      title: "Kingland",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      bgUrl: "/assets/images/our_games/2_kingland.png",
+    },
+    {
+      id: 3,
+      title: "The Last Game",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      bgUrl: "/assets/images/our_games/3_the_last_game.png",
+    },
+    {
+      id: 4,
+      title: "G-Dragon",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      bgUrl: "/assets/images/our_games/4_g_dragon.png",
+    },
+    {
+      id: 5,
+      title: "Pirates",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      bgUrl: "/assets/images/our_games/5_pirates.png",
+    },
+    {
+      id: 6,
+      title: "Witch Party",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      bgUrl: "/assets/images/our_games/6_witch_party.png",
+    },
+    {
+      id: 7,
+      title: "Rocky",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      bgUrl: "/assets/images/our_games/7_rocky.png",
+    },
+    {
+      id: 8,
+      title: "Blue Fire",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      bgUrl: "/assets/images/our_games/8_blue_fire.png",
+    },
+    {
+      id: 9,
+      title: "Magic tree",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      bgUrl: "/assets/images/our_games/9_magic_tree.png",
+    },
+    {
+      id: 10,
+      title: "Aborigines",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      bgUrl: "/assets/images/our_games/10_aborigines.png",
+    },
+    {
+      id: 11,
+      title: "Cinderella",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      bgUrl: "/assets/images/our_games/11_cinderella.png",
+    },
+    {
+      id: 12,
+      title: "Egypt Game",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      bgUrl: "/assets/images/our_games/12_egypt_game.png",
+    },
+  ];
   return (
-    <div>OurGames</div>
-  )
-}
+    <div className="w-full px-20">
+      <SectionHeader
+        title="Our Games"
+        desc="As a pioneer of mobile app gamification, we take pride in originality and individuality, providing global players with state-of-the-art games that feature splendid storylines, sensational sound effects and magnificent animation that never cease to impress."
+        className="text-center max-w-[860px] m-auto"
+      />
+      <div className="grid grid-cols-4 gap-4 mt-20">
+        {data.map((item, index) => (
+          <div
+            key={index}
+            className={`relative group rounded-lg overflow-hidden ${
+              index % 4 === 1 || index % 4 === 3 ? "top-[50px]" : ""
+            }`}
+          >
+            <Image
+              src={item.bgUrl}
+              alt={"bg"}
+              width={410}
+              height={560}
+              className="w-full h-auto transform transition-transform duration-300 group-hover:scale-110"
+            />
+            <div className="absolute inset-0 flex flex-col justify-end p-4 bg-gradient-to-t from-black via-transparent to-transparent opacity-0 transition-opacity duration-300 group-hover:opacity-100 group-hover:bg-[linear-gradient(360deg,_rgba(0,9,225,0.5)_6.79%,_rgba(0,0,0,0)_61.34%)] mix-blend-mode-normal">
+              <h2 className="text-white text-5xl font-bold">{item.title}</h2>
+              <p className="text-white text-sm">{item.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default OurGames
+export default OurGames;
--- src/components/SectionHeader/index.tsx
+++ src/components/SectionHeader/index.tsx
@@ -3,15 +3,16 @@ import React from "react";
 type SectionHeaderProps = {
   title: string;
   desc?: string;
+  className?: string;
 };
 
-const SectionHeader = ({ title, desc }: SectionHeaderProps) => {
+const SectionHeader = ({ title, desc, className }: SectionHeaderProps) => {
   return (
-    <div>
-      <h3 className="text-[60px] font-black font-playfair text-black">
+    <div className={className}>
+      <h3 className={`text-[60px] font-black font-playfair text-black`}>
         {title}
       </h3>
-      {desc && <p className="pt-10 text-[#757575] text-sm">{desc}</p>}
+      {desc && <p className={`pt-10 text-[#757575] text-sm`}>{desc}</p>}
     </div>
   );
 };
